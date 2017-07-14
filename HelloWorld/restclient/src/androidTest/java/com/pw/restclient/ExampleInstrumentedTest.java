6ecbcb08058a81f684e8ccbb55b00391d15fa737
Tom Reed <t.reed@waracle.com>
2017-07-14 16:04:38 +0100

    Library name change

--- HelloWorld/restclient/src/androidTest/java/com/pw/restclient/ExampleInstrumentedTest.java
+++ HelloWorld/restclient/src/androidTest/java/com/pw/restclient/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.pw.restlibrary.test", appContext.getPackageName());
+        assertEquals("com.pw.restclient.test", appContext.getPackageName());
     }
 }
